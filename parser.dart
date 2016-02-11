ec72ddb7d7ccc0ad5c12daf7fb5e4b2c18287dd8
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2016-02-11 20:21:15 +0100

    Create parser method which will return simulation.

--- parser.dart
+++ parser.dart
@@ -1,6 +1,30 @@
+import 'dart:io';
+
+import 'Map.dart';
+import 'Drone.dart';
+
 class Simulation {
     Map map;
 
     List<WareHouse> warehouses;
+    List<Drone> drones;
+    List<Customers> customers;
 
+    Simulation();
+}
+
+void createMap(Simulation sim, List<String> lines) {
+    sim.map = new Map();
+}
+
+Simulation parse(File input) {
+    Simultation sim = new Simulation();
+    List<String> lines = input.readAsLinesSync();
+    List<String> mapParams = lines[0].split(' ');
+
+    sim.map = new Map(int.parse(mapParams[1]), int.parse(mapParams[0]));
+
+    print(sim.map.width);
+
+    return sim;
 }
